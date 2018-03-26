bd2539b675d1853f923807e9d02c759cbaac291e
Abe Queen <abedababe8@gmail.com>
2018-03-26 14:40:12 -0700

    formatting and renaming

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  const meat = document.querySelector('.meat')
+  const productsGrid = document.querySelector('.products-grid')
 
   const grid = document.createElement('div')
   grid.classList.add('grid')
-  meat.appendChild(grid)
+  productsGrid.appendChild(grid)
 
   for (var numOfGridItems = 0; numOfGridItems < 8; numOfGridItems++) {
     const gridItem = document.createElement('div')
     gridItem.classList.add('grid-item')
     const gridInner = document.createElement('div')
     gridInner.classList.add('grid-inner')
-    gridInner.innerHTML = numOfGridItems + 1
+    const gridInnerText = document.createElement('div')
+    const gridInnerImg = document.createElement('div')
+    gridInner.appendChild(gridInnerText)
+    gridInner.appendChild(gridInnerText)
     gridItem.appendChild(gridInner)
     grid.appendChild(gridItem)
   }
