50197e5c0b0d2f10003f6e3a0e450ac17a6b2f0e
Abe Queen <abedababe8@gmail.com>
2018-03-26 16:55:14 -0700

    product page structure and grid set

---
+++ javaScript/data.js
@@ -0,0 +1,40 @@
+productList = [{
+    name: 'Rocky Road',
+    img:'https://cdn.shopify.com/s/files/1/0377/2037/products/WhiteTanLeather.Front_1024x.jpg?v=1510683461',
+    price: 7.00
+  },
+  {
+    name: 'Strawberry Sunset',
+    img:'https://cdn.shopify.com/s/files/1/0377/2037/products/Womens03.Front_ROSE_GOLD_ADD_grande.jpg?v=1510687973',
+    price: 7.00
+  },
+  {
+    name: 'Snoqualimie Sugar',
+    img:'https://cdn.shopify.com/s/files/1/0377/2037/products/Mens36.Front_5a287144-63e8-4254-bef0-450a68ccd268_grande.jpg?v=1510684704',
+    price: 7.00
+  },
+  {
+    name: 'Vanilla Voyage',
+    img:'https://www.edmondsjewellers.com/images/rotary-mens-rose-gold-square-windsor-watch-p7864-17807_image.jpg',
+    price: 7.00
+  },
+  {
+    name: 'Mint Mountain',
+    img: 'https://cdn.shopify.com/s/files/1/0377/2037/products/Voyager08.Front_1024x.jpg?v=1510685016',
+    price: 7.00
+  },
+  {
+    name: 'Blueberry Blush',
+    img: 'https://s-media-cache-ak0.pinimg.com/originals/67/56/44/6756448395707d8ea38b55fa9ce650f7.jpg',
+    price: 7.00
+  },
+  {
+    name: "Cookies 'n' Cream",
+    img: 'http://cdn.watchshop.com/profiler/thumb_cache/zoom/100001793_v_1465558488.jpg',
+    price: 7.00
+  },
+  {
+    name: "Cookies 'n' Cream",
+    img: 'https://fossil.scene7.com/is/image/FossilPartners/ME3061_main?&scl=1',
+    price: 7.00
+  }]
