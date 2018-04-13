5e74967229df569d812331df8feb88171d5cf7b7
bryankai <brykai123@gmail.com>
2018-04-13 09:23:29 -0700

    Added email subscribe alert popup when email is correct

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
 
   let totalArr = []
@@ -6,22 +6,22 @@ document.addEventListener('DOMContentLoaded', function(){
   const navBrand = document.createElement('div')
   navBrand.classList.add('navBrand')
   const navOptions = document.createElement('div')
-    navOptions.classList.add('navOptions')
+  navOptions.classList.add('navOptions')
   const navLogo = document.createElement('img')
-    navLogo.setAttribute('src', './photos/WatchIcon.png')
-    navLogo.classList.add('navLogo')
+  navLogo.setAttribute('src', './photos/WatchIcon.png')
+  navLogo.classList.add('navLogo')
   const navTitle = document.createElement('a')
-    navTitle.classList.add('navTitle')
-    navTitle.setAttribute('href', '/index.html')
-    navTitle.innerHTML = 'Just in Time'
+  navTitle.classList.add('navTitle')
+  navTitle.setAttribute('href', '/index.html')
+  navTitle.innerHTML = 'Just in Time'
   const homeLink = document.createElement('a')
-    homeLink.classList.add('navLink')
-    homeLink.setAttribute('href', '/index.html')
-    homeLink.innerHTML = '| Home'
+  homeLink.classList.add('navLink')
+  homeLink.setAttribute('href', '/index.html')
+  homeLink.innerHTML = '| Home'
   const productsLink = document.createElement('a')
-    productsLink.classList.add('navLink')
-    productsLink.innerHTML = '| Products |'
-    productsLink.setAttribute('href', '/products.html')
+  productsLink.classList.add('navLink')
+  productsLink.innerHTML = '| Products |'
+  productsLink.setAttribute('href', '/products.html')
 
   navBrand.appendChild(navLogo)
   navBrand.appendChild(navTitle)
@@ -34,10 +34,17 @@ document.addEventListener('DOMContentLoaded', function(){
 
   const cart = document.createElement('a')
   navOptions.appendChild(cart)
-    cart.innerHTML = `<i class="fas fa-shopping-cart"></i>`
-    cart.classList.add('cart')
-    cart.addEventListener('click', event => {
-
+  cart.innerHTML = `<i class="fas fa-shopping-cart"></i>`
+  cart.classList.add('cart')
+  cart.addEventListener('click', event => {
     renderCart()
+  })
+  // Email Subscription Popup
+  const form = document.querySelector("form")
+  const emailEntry = document.getElementById("email-signup")
+  form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    emailEntry.value =""
+    alert('Subscribed!')
   })
 })
